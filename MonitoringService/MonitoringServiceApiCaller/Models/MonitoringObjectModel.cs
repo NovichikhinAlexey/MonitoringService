66530e05402c3644d5e3f9f60822e58334ed8a87
AleksandrBelousov <tortyt1@gmail.com>
2017-05-22 11:07:11 +0400

    Renamed Nuget package and added dockerfile for api runner

--- MonitoringService/MonitoringServiceApiCaller/Models/MonitoringObjectModel.cs
+++ MonitoringService/MonitoringServiceApiCaller/Models/MonitoringObjectModel.cs
@@ -2,7 +2,7 @@
 // Changes may cause incorrect behavior and will be lost if the code is
 // regenerated.
 
-namespace MonitoringServiceApiCaller.Models
+namespace Lykke.MonitoringServiceApiCaller.Models
 {
     using Newtonsoft.Json;
     using System.Linq;
